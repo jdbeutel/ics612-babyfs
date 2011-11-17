553ae9d051e1ebb2393c08abba04d7647fff547b
J. David Beutel <git@getsu.com>
2011-11-16 23:36:22 -1000

    first draft of my_mkfs(), not compiled

--- babyfs.h
+++ babyfs.h
@@ -3,64 +3,90 @@
 #include <stdint.h>
 #include <time.h>
 
-typedef uint32_t blocknr_t;
-typedef uint16_t block_offset_t;
-typedef uint16_t block_size_t;
+typedef uint32_t blocknr_t;	/* need 21 bits for 2G in 1K blocks */
+typedef uint16_t item_offset_t;	/* need 10 bits for bytes within a block */
+typedef uint16_t item_size_t;	/* need 10 bits for bytes within a block */
 
-struct babyfs_superblock {	/* first block of device */
-	uint32_t magic_number;		/* "baby" */
-	blocknr_t extent_tree_blocknr;	/* root */
-	blocknr_t fs_tree_blocknr;	/* root */
-	uint32_t total_blocks;	/* device size */
-}
+/* nodes and keys in FS tree */
+#define TYPE_INODE		10
+#define TYPE_DIR_ENT		11
+#define TYPE_FILE_EXTENT	12
 
-struct babyfs_header {		/* at start of all nodes in tree */
-	blocknr_t blocknr;
-	uint8_t flags;
+/* nodes and keys in extent tree */
+#define TYPE_SUPERBLOCK		20
+#define TYPE_EXT_IDX		21
+#define TYPE_EXT_LEAF		22
+#define TYPE_FS_IDX		23
+#define TYPE_FS_LEAF		24
+#define TYPE_FILE_DATA		25
+
+struct header {			/* starts all nodes in a tree */
+	blocknr_t blocknr;	/* for testing and simple consistency check */
+	uint8_t type;		/* for testing, redundant with key.type */
 	uint8_t nritems;	/* populated key or item slots */
 	uint8_t level;		/* number of nodes to get to root */
 }
 
-struct babyfs_key {		/* in index and leaf nodes */
+struct key {		/* in index and leaf nodes */
 	uint32_t objectid;	/* inode or blocknr */
 	uint8_t type;
 	uint32_t offset;
 }
 
-#define BABYFS_TYPE_INODE	10
-#define BABYFS_TYPE_DIR_ENT	11
-#define BABYFS_TYPE_FILE_EXTENT	12
-
-#define BABYFS_TYPE_SUPERBLOCK	20
-#define BABYFS_TYPE_EXT_IDX	21
-#define BABYFS_TYPE_EXT_LEAF	22
-#define BABYFS_TYPE_FS_IDX	23
-#define BABYFS_TYPE_FS_LEAF	24
-#define BABYFS_TYPE_FILE_DATA	25
-
-struct babyfs_key_ptr {		/* in index nodes */
-	struct babyfs_key key;
+struct key_ptr {		/* in index nodes */
+	struct key key;
 	blocknr_t blocknr;
 }
 
-struct babyfs_item {		/* in leaf node */
-	struct babyfs_key key;
-	block_offset_t offset;	/* of metadata, in bytes from start of block */
-	block_size_t size;	/* of metadata, in bytes */
+#define NODE_PAYLOAD_BYTES	(BLOCKSIZE-sizeof(struct header))
+#define MAX_KEY_PTRS	(NODE_PAYLOAD_BYTES/sizeof(struct key_ptr))
+struct index_node {
+	struct header;
+	struct key_ptr key_ptrs[MAX_KEY_PTRS];
 }
 
-struct babyfs_inode_metadata {
+struct item {		/* in leaf node */
+	struct key key;
+	item_offset_t offset;	/* of metadata, in bytes from start of block */
+	item_size_t size;	/* of metadata, in bytes */
+}
+
+#define MAX_ITEMS	(NODE_PAYLOAD_BYTES/sizeof(struct item))
+struct leaf_node {
+	struct header;
+	struct item items[MAX_ITEMS];
+}
+
+#define INODE_DIR	1
+#define INODE_FILE	2
+struct inode_metadata {
 	uint8_t inode_type;
 	time_t ctime;
 	time_t mtime;
 }
+#define ROOT_DIR_INODE		0
+#define INODE_KEY_OFFSET	0
 
-struct babyfs_dir_ent_metadata {
+struct dir_ent_metadata {
 	uint32_t inode;
 	char name[1]; /* variable-sized, \0-terminated, up to 200 chars */
 }
 
-struct babyfs_file_extent_metadata {
+struct file_extent_metadata {
 	blocknr_t blocknr;
 	uint32_t size;	/* in bytes */
+}
+
+#define MIN_LOWER_BOUNDS	2
+#define MAX_LOWER_BOUNDS	(MAX_KEY_PTRS/3)
+#define MAGIC_NUMBER	0xBABF
+#define BABYFS_VERSION	0
+
+struct superblock {	/* first block of device */
+	uint32_t magic_number;
+	uint8_t version;
+	blocknr_t extent_tree_blocknr;	/* root */
+	blocknr_t fs_tree_blocknr;	/* root */
+	int total_blocks;	/* device size */
+	uint8_t lower_bounds;	/* b for balancing inner nodes b..3b */
 }
--- p6.c
+++ p6.c
@@ -1,3 +1,5 @@
+/* ICS612 proj6 jbeutel 2011-11-16 */
+
 /* project 6 file for UHM ICS 612, Spring 2007*/
 /* This program is public domain.  As written, it does very little.
  * You are welcome to use it and modify it as you see fit, as long
@@ -69,6 +71,127 @@ int my_rmdir (char * path)
  * and if not, create one. */
 void my_mkfs ()
 {
-  printf ("my_mkfs not implemented\n");
-}
+	block super_check, blocks[5];
+	struct superblock *super;
+	struct index_node *index;
+	struct leaf_node *leaf;
+	int devsize, i;
 
+	devsize = dev_open();
+	if (devsize < 0) return;
+
+	if (read_block(0, super_check)) return;
+	super = super_check;
+	if (super->magic_number == MAGIC_NUMBER) {
+		fprintf(stderr, "device already has this file system\n");
+		return;
+	}
+	if (devsize < 5) {
+		fprintf(stderr, "device too small (%d blocks)\n", devsize);
+		return;
+	}
+	super = blocks[0];
+	super->magic_number = MAGIC_NUMBER;
+	super->version = BABYFS_VERSION;
+	super->extent_tree_blocknr = 1;
+	super->fs_tree_blocknr = 3;
+	super->total_blocks = devsize;
+	super->lower_bounds = MIN_LOWER_BOUNDS;	/* min for testing tree ops */
+
+	index = blocks[1];
+	index->header.blocknr = 1;
+	index->header.type = TYPE_EXT_IDX;
+	index->header.nritems = 5;
+	index->header.level = 0;	/* root node */
+
+	/* todo: bootstrap root node and then add these via basic tree ops */
+	index->key_ptrs[0].key.objectid = 0;
+	index->key_ptrs[0].key.type = TYPE_SUPERBLOCK;
+	index->key_ptrs[0].key.offset = 1;
+	index->key_ptrs[0].blocknr = 2;
+
+	index->key_ptrs[1].key.objectid = 1;
+	index->key_ptrs[1].key.type = TYPE_EXT_IDX;
+	index->key_ptrs[1].key.offset = 1;
+	index->key_ptrs[1].blocknr = 2;
+
+	index->key_ptrs[2].key.objectid = 2;
+	index->key_ptrs[2].key.type = TYPE_EXT_LEAF;
+	index->key_ptrs[2].key.offset = 1;
+	index->key_ptrs[2].blocknr = 2;
+
+	index->key_ptrs[3].key.objectid = 3;
+	index->key_ptrs[3].key.type = TYPE_FS_IDX;
+	index->key_ptrs[3].key.offset = 1;
+	index->key_ptrs[3].blocknr = 2;
+
+	index->key_ptrs[4].key.objectid = 4;
+	index->key_ptrs[4].key.type = TYPE_FS_LEAF;
+	index->key_ptrs[4].key.offset = 1;
+	index->key_ptrs[4].blocknr = 2;
+
+	leaf = blocks[2];
+	leaf->header.blocknr = 2;
+	leaf->header.type = TYPE_EXT_LEAF;
+	leaf->header.nritems = 5;
+	leaf->header.level = 1;
+
+	leaf->items[0].key.objectid = 0;
+	leaf->items[0].key.type = TYPE_SUPERBLOCK;
+	leaf->items[0].key.offset = 1;
+	leaf->items[0].offset = BLOCKSIZE;	/* not in block */
+	leaf->items[0].size = 0;
+
+	leaf->items[1].key.objectid = 1;
+	leaf->items[1].key.type = TYPE_EXT_IDX;
+	leaf->items[1].key.offset = 1;
+	leaf->items[1].offset = BLOCKSIZE;	/* not in block */
+	leaf->items[1].size = 0;
+
+	leaf->items[2].key.objectid = 2;
+	leaf->items[2].key.type = TYPE_EXT_LEAF;
+	leaf->items[2].key.offset = 1;
+	leaf->items[2].offset = BLOCKSIZE;	/* not in block */
+	leaf->items[2].size = 0;
+
+	leaf->items[3].key.objectid = 3;
+	leaf->items[3].key.type = TYPE_FS_IDX;
+	leaf->items[3].key.offset = 1;
+	leaf->items[3].offset = BLOCKSIZE;	/* not in block */
+	leaf->items[3].size = 0;
+
+	leaf->items[4].key.objectid = 4;
+	leaf->items[4].key.type = TYPE_FS_LEAF;
+	leaf->items[4].key.offset = 1;
+	leaf->items[4].offset = BLOCKSIZE;	/* not in block */
+	leaf->items[4].size = 0;
+
+	index = blocks[3];
+	index->header.blocknr = 3;
+	index->header.type = TYPE_FS_IDX;
+	index->header.nritems = 1;
+	index->header.level = 0;	/* root node */
+
+	index->key_ptrs[0].key.objectid = ROOT_DIR_INODE;	/* root dir */
+	index->key_ptrs[0].key.type = TYPE_INODE;
+	index->key_ptrs[0].key.offset = INODE_KEY_OFFSET;
+	index->key_ptrs[0].blocknr = 4;
+
+	leaf = blocks[4];
+	leaf->header.blocknr = 4;
+	leaf->header.type = TYPE_FS_LEAF;
+	leaf->header.nritems = 1;
+	leaf->header.level = 1;
+
+	leaf->items[0].key.objectid = ROOT_DIR_INODE;
+	leaf->items[0].key.type = TYPE_INODE;
+	leaf->items[0].key.offset = INODE_KEY_OFFSET;
+	leaf->items[0].offset = BLOCKSIZE;	/* not in block */
+	leaf->items[0].size = 0;
+
+	for (i = 1; i < sizeof(blocks); i++) {
+		if (write_block(i, block[i])) return;
+	}
+	/* write superblock last */
+	write_block(0, block[0]);
+}
